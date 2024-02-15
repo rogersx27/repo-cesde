96ada6ea3c7dea95ba6053697736893e88114a51
Juan Pablo Guevara López <jpguevara909@soy.sena.edu.co>
2024-02-15 18:14:47 -0500

    update seeder and logic in all files

--- python/AgenciaViajes/database_manager.py
+++ python/AgenciaViajes/database_manager.py
@@ -13,10 +13,19 @@ class DatabaseManager:
     def create_connection(self):
         try:
             self.conn = sqlite3.connect(self.db_file)
+            # Creamos las tablas al iniciar la conexión
+            self.create_table_destinations()  # <- Llama a la función para crear la tabla de destinos
+            self.create_table_clients()  # <- Llama a la función para crear la tabla de clientes
         except Error as e:
             print(e)
 
-    def create_table(self):
+    def close_connection(self):
+        if self.conn:
+            self.conn.close()
+
+    # Tabla de destinos #
+
+    def create_table_destinations(self):
         try:
             cur = self.conn.cursor()
 
@@ -32,14 +41,14 @@ class DatabaseManager:
         except Error as e:
             print(e)
 
-    def insert_data(self, data):
+    def insert_data_destinations(self, data):
         sql = ''' INSERT INTO AgenciaViajes(destino, vlr_personaAdulta, vlr_personaMenor)
                   VALUES(?,?,?) '''
         cur = self.conn.cursor()
         cur.execute(sql, data)
         self.conn.commit()
 
-    def update_data(self, data):
+    def update_data_destinations(self, data):
         sql = ''' UPDATE AgenciaViajes
                   SET destino = ? ,
                       vlr_personaAdulta = ? ,
@@ -49,20 +58,80 @@ class DatabaseManager:
         cur.execute(sql, data)
         self.conn.commit()
 
-    def delete_data(self, id):
+    def delete_data_destinations(self, id):
         sql = 'DELETE FROM AgenciaViajes WHERE id = ?'
         cur = self.conn.cursor()
         cur.execute(sql, (id,))
         self.conn.commit()
 
-    def select_all_data(self):
+    def select_all_data_destinations(self):
         df = pd.read_sql_query("SELECT * FROM AgenciaViajes", self.conn)
         return df
 
-    def select_data(self, id):
+    def select_data_destinations(self, id):
         df = pd.read_sql_query(f"SELECT * FROM AgenciaViajes WHERE id = {id}", self.conn)
         return df
 
-    def close_connection(self):
-        if self.conn:
-            self.conn.close()
+    def select_data_destinations_by_name(self, name):
+        df = pd.read_sql_query(f"SELECT * FROM AgenciaViajes WHERE destino = '{name}'", self.conn)
+        return df
+
+    # Tabla de clientes #
+
+    def create_table_clients(self):
+        try:
+            cur = self.conn.cursor()
+
+            sql_create_clients_table = """
+                            CREATE TABLE IF NOT EXISTS Personas (
+                                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                nombre TEXT NOT NULL,
+                                apellido TEXT NOT NULL,
+                                id_viaje INTEGER,
+                                nro_adultos INTEGER NOT NULL,
+                                nro_ninos INTEGER NOT NULL,
+                                subtotal REAL NOT NULL,
+                                FOREIGN KEY (id_viaje) REFERENCES AgenciaViajes(id)
+                            )
+                        """
+            cur.execute(sql_create_clients_table)
+        except Error as e:
+            print(e)
+
+    def insert_persona_data(self, data):
+        sql = ''' INSERT INTO Personas(nombre, apellido, id_viaje, nro_adultos, nro_ninos, subtotal)
+                  VALUES(?, ?, ?, ?, ?, ?) '''
+        cur = self.conn.cursor()
+        cur.execute(sql, data)
+        self.conn.commit()
+
+    def update_persona_data(self, data):
+        sql = ''' UPDATE Personas
+                  SET nombre = ? ,
+                      apellido = ? ,
+                      id_viaje = ? ,
+                      nro_adultos = ? ,
+                      nro_ninos = ? ,
+                      subtotal = ?
+                  WHERE id = ?'''
+        cur = self.conn.cursor()
+        cur.execute(sql, data)
+        self.conn.commit()
+
+    def delete_persona_data(self, id):
+        sql = 'DELETE FROM Personas WHERE id = ?'
+        cur = self.conn.cursor()
+        cur.execute(sql, (id,))
+        self.conn.commit()
+
+    def select_all_personas_data(self):
+        df = pd.read_sql_query("SELECT * FROM Personas", self.conn)
+        return df
+
+    def select_persona_data(self, id):
+        df = pd.read_sql_query(f"SELECT * FROM Personas WHERE id = {id}", self.conn)
+        return df
+
+    def select_personas_data(self, id_viaje):
+        df = pd.read_sql_query(f"SELECT * FROM Personas WHERE id_viaje = {id_viaje}", self.conn)
+        return df
--- python/AgenciaViajes/ejrc_AgenciaViajes.py
+++ python/AgenciaViajes/ejrc_AgenciaViajes.py
@@ -1,59 +1,127 @@
 from tabulate import tabulate
+from time import sleep
 
 from database_manager import DatabaseManager
 
+# Inicializar la base de datos
+database = r"./db/AgenciaViajes.db"
+db_manager = DatabaseManager(database)
+
+
+def consult_all_data():
+    # Consultar todos los datos
+    all_data = db_manager.select_all_data_destinations()
+    destinations = all_data['destino'].values.tolist()  # <- Obtener todos los destinos y guardarlos en una lista
+
+    # Crear una lista de listas con los datos formateados
+    table_allData = [[f"{i + 1}", destination] for i, destination in enumerate(destinations)]
+
+    # Imprimir la tabla formateada
+    print(tabulate(table_allData, headers=["ID", "Destinos"], tablefmt="pretty"))
+
+
+def consult_specific_data():
+    # Consultar un dato específico
+    id = int(input("Ingrese el id del dato que desea consultar: "))
+    data = db_manager.select_data_destinations(id)  # <- Consultar un dato
+
+    # Crear una lista de listas con los datos formateados
+    table_data = [
+        [data['destino'].values[0], data['vlr_personaAdulta'].values[0], data['vlr_personaMenor'].values[0]]
+    ]
+    print(tabulate(table_data, headers=["Destino", "Valor persona adulta", "Valor persona menor"],
+                   tablefmt="pretty"))
+
+
+def do_quote():
+    # Realizar cotización
+    # Pedir todos los datos necesarios para realizar la consulta y posible inserción a la base de datos
+    trip_id = int(input("Ingrese el id del destino: "))
+    adults = int(input("Ingrese la cantidad de personas adultas: "))
+    children = int(input("Ingrese la cantidad de personas menores: "))
+
+    data = db_manager.select_data_destinations(trip_id)  # <- Consultar un dato
+    values = [data['vlr_personaAdulta'].values[0], data['vlr_personaMenor'].values[0]]  # <- Guardamos los precios
+
+    total = float((adults * values[0]) + (children * values[1]))  # <- Calculamos el total
+
+    table_data = [
+        [data['destino'].values[0], adults, children, total]
+    ]
+
+    print(tabulate(table_data, headers=["Destino", "Cantidad de adultos", "Cantidad de menores", "Total"],
+                   tablefmt="pretty"))
+
+    # Preguntar si desea guardar la cotización
+    save = input("¿Desea guardar la cotización? (s/n): ")
+    if save.lower() == "s":
+        # Pedimos los datos de la persona que realiza la cotización
+        nombre = str(input("Ingrese su nombre: "))
+        apellido = str(input("Ingrese su apellido: "))
+
+        db_manager.insert_persona_data([nombre, apellido, trip_id, adults, children, total])  # <- Guardar la cotización
+        print("Cotización guardada.")
+    else:
+        print("Cotización no guardada.")
+
 
 def menu():
-    database = r"./db/AgenciaViajes.db"
-    db_manager = DatabaseManager(database)
-
     while True:
         print("""
         Menú principal
         ----------------------------
         1. Consultar todos los destinos.
         2. Consultar un dato específico.
-        3. Insertar un dato.
-        4. Actualizar un dato.
-        5. Eliminar un dato.
-        6. Salir.
+        3. Realizar cotización.
+        4. Panel de administración.
+        0. Salir.
         ----------------------------
         """)
 
         menu = int(input())
 
         if menu == 1:
-            # Consultar todos los datos
-            all_data = db_manager.select_all_data()
-            destinations = all_data['destino'].values.tolist()  # <- Obtener los destinos
+            consult_all_data()
 
-            # Crear una lista de listas con los datos formateados
-            table_allData = [[f"{i + 1}", destination] for i, destination in enumerate(destinations)]
-
-            # Imprimir la tabla formateada
-            print(tabulate(table_allData, headers=["ID", "Destinos"], tablefmt="pretty"))
         elif menu == 2:
-            # Consultar un dato específico
-            id = int(input("Ingrese el id del dato que desea consultar: "))
-            data = db_manager.select_data(id)  # <- Consultar el dato
-
-            # Crear una lista de listas con los datos formateados
-            table_data = [
-                [data['destino'].values[0], data['vlr_personaAdulta'].values[0], data['vlr_personaMenor'].values[0]]]
-            print(tabulate(table_data, headers=["Destino", "Valor persona adulta", "Valor persona menor"],
-                           tablefmt="pretty"))
+            consult_specific_data()
 
         elif menu == 3:
-        # TODO: Implementar la inserción de un dato
+            do_quote()
 
         elif menu == 4:
-        # TODO: Implementar la actualización de un dato
-
-        elif menu == 5:
-        # TODO: Implementar la eliminación de un dato
-
-        elif menu == 6:
-            print("Hasta luego.")
+            print("Ingrese la contraseña de administrador: ")
+            password = input()
+            if password == "admin":
+                while True:
+                    print("""
+                    Panel de administración
+                    ----------------------------
+                    1. Consultar todas las cotizaciones.
+                    2. Consultar una cotización específica.
+                    3. Eliminar una cotización.
+                    0. Salir.
+                    ----------------------------
+                    """)
+                    menu_admin = int(input())
+                    if menu_admin == 1:
+                        all_quotes = db_manager.select_all_personas_data()
+                        table_allQuotes = [[f"{all_quotes['id'].values[i]}", all_quotes['nombre'].values[i], all_quotes['apellido'].values[i], all_quotes['id_viaje'].values[i], all_quotes['nro_adultos'].values[i], all_quotes['nro_ninos'].values[i], all_quotes['subtotal'].values[i]] for i in range(len(all_quotes))]
+                        print(tabulate(table_allQuotes, headers=["ID", "Nombre", "Apellido", "ID Viaje", "Nro. adultos", "Nro. niños", "Subtotal"], tablefmt="pretty"))
+                    elif menu_admin == 2:
+                        id_quote = int(input("Ingrese el ID de la cotización: "))
+                        quote = db_manager.select_persona_data(id_quote)
+                        # trip = db_manager.select_data_destinations(quote['id_viaje'].values[0].tolist())
+                        trip_id_name = (db_manager.select_data_destinations(quote['id_viaje'].values[0].tolist()))['destino'].values[0]
+                        # print(trip)
+                        # print(trip_id_name)
+                        table_quote = [[quote['nombre'].values[0], quote['apellido'].values[0], trip_id_name, quote['nro_adultos'].values[0], quote['nro_ninos'].values[0], quote['subtotal'].values[0]]]
+                        print(tabulate(table_quote, headers=["Nombre", "Apellido", "Viaje a", "Nro. adultos", "Nro. niños", "Subtotal"], tablefmt="pretty"))
+        elif menu == 0:
+            print("Cerrando programa...")
+            db_manager.close_connection()
+            sleep(3)
+            print("Programa cerrado. ¡Hasta luego!")
             break
 
 
